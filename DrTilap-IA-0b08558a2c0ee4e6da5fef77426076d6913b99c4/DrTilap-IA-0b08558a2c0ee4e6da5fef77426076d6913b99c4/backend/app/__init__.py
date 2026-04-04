b4835bce7bc592d647a694fc96f62667a0b05443
Fernando <ffasti.ia01@gmail.com>
2026-04-04 17:20:19 -0300

    Login funcionando

--- DrTilap-IA-0b08558a2c0ee4e6da5fef77426076d6913b99c4/DrTilap-IA-0b08558a2c0ee4e6da5fef77426076d6913b99c4/backend/app/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Pacote principal da aplicação DrTilápia 1.3.
-Centraliza importações de módulos críticos.
-"""
-
-__version__ = "2.0"
--- DrTilap-IA-0b08558a2c0ee4e6da5fef77426076d6913b99c4/DrTilap-IA-0b08558a2c0ee4e6da5fef77426076d6913b99c4/backend/app/auth/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Módulo de autenticação do DrTilápia 2.0.
-"""
